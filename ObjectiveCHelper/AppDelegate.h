8fdd451a1e681f8d1760e8365c199ba92bd6122c
teenloong <634205468@qq.com>
2022-06-07 09:55:43 +0800

    Initial Commit

---
+++ ObjectiveCHelper/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjectiveCHelper
+//
+//  Created by qfdev on 2022/6/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjectiveCHelper/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ObjectiveCHelper
+//
+//  Created by qfdev on 2022/6/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
