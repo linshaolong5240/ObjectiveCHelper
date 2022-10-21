73a341547894221519320742e0cdfb9b3b2ae93a
Sauron <634205468@qq.com>
2022-10-21 14:50:58 +0800

    UIButtonDemoViewController

--- ObjectiveCHelper/UIKitDemo/Controller/UIButtonDemoViewController.m
+++ ObjectiveCHelper/UIKitDemo/Controller/UIButtonDemoViewController.m
@@ -38,6 +38,8 @@
         [textSymbolButton setImage:[UIImage systemImageNamed:@"chevron.right"] forState:UIControlStateNormal];
         textSymbolButton.semanticContentAttribute = UISemanticContentAttributeForceRightToLeft;
 //        textSymbolButton.titleLabel.backgroundColor = UIColor.greenColor;
+        
+        //不加背景色显示会有问题！
         textSymbolButton.imageView.backgroundColor = UIColor.clearColor;
 
         textSymbolButton.imageEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
